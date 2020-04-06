5f8ae8c0bb1a9710a093a88533aea67a133fc5d7
Andrew Steinmeyer <jandrewsteinmeyer@gmail.com>
2020-04-06 15:09:05 -0400

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -187,7 +187,7 @@ const IndexPage = () => (
         <div className="contactArea">
           <div className="columns is-centered">
             <div className="column is-two-fifths contact-column animated fade-in-up">
-              <form method="post" className="contact-form" name="contact" netlify-honeypot="bot-field" data-netlify="true">
+              <form method="post" className="contact-form" name="contact" data-netlify="true">
                 <input placeholder="Name" type="text" name="name" required></input>
                 <input placeholder="Enter email" type="email" name="email" required></input>
                 <textarea placeholder="Your Message" type="text" name="message" required></textarea>
