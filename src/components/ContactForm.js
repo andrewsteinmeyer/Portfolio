c5db66553ff0039ba6565a57ee71c73834c5bec1
Andrew Steinmeyer <jandrewsteinmeyer@gmail.com>
2020-04-06 16:33:45 -0400

    adding contact form file

---
+++ src/components/ContactForm.js
@@ -0,0 +1,58 @@
+
+import React from 'react'
+import navigate from 'gatsby-link'
+
+function encode(data) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
+export default function Contact() {
+  const [state, setState] = React.useState({})
+
+  const handleChange = (e) => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const form = e.target
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({
+        'form-name': form.getAttribute('name'),
+        ...state,
+      }),
+    })
+      .then(() => navigate(form.getAttribute('action')))
+      .catch((error) => alert(error))
+  }
+
+  return (
+      <form
+        className="contact-form"
+        name="contact"
+        method="post"
+        action="/thanks/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
+      >
+        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label>
+            Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+          </label>
+        </p>
+        <input placeholder="Name" type="text" name="name" required></input>
+        <input placeholder="Enter email" type="email" name="email" required></input>
+        <textarea placeholder="Your Message" type="text" name="message" required></textarea>
+        <input className="button submit-form is-pulled-right" type="submit" id="submit" value="SUBMIT"></input>
+      </form>
+  )
+}
+
+
