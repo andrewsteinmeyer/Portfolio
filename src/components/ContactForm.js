210d2c4c6c2e342208c72a74e805d2c3b14b9646
Andrew Steinmeyer <jandrewsteinmeyer@gmail.com>
2020-04-07 10:43:07 -0400

    Updating alert for contact form and moving contact form css

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,58 +1,66 @@
 
 import React from 'react'
-import navigate from 'gatsby-link'
+import $ from 'jquery'
 
-function encode(data) {
-  return Object.keys(data)
-    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-    .join('&')
+import './ContactForm.css'
+
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
 }
 
-export default function Contact() {
-  const [state, setState] = React.useState({})
+export default class ContactForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { name: "", email: "", message: "" };
+        }
 
-  const handleChange = (e) => {
-    setState({ ...state, [e.target.name]: e.target.value })
-  }
+    handleSubmit = e => {
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", ...this.state })
+        })
+            .then(() => {
+                $('#success').each(function() {
+                    $(this).addClass('expand');
+                });
+            })
+            .catch(error => alert(error));
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const form = e.target
-    fetch('/', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      body: encode({
-        'form-name': form.getAttribute('name'),
-        ...state,
-      }),
-    })
-      .then(() => navigate(form.getAttribute('action')))
-      .catch((error) => alert(error))
-  }
+        e.preventDefault();
+        };
 
-  return (
-      <form
-        className="contact-form"
-        name="contact"
-        method="post"
-        action="/thanks/"
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        onSubmit={handleSubmit}
-      >
-        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-        <input type="hidden" name="form-name" value="contact" />
-        <p hidden>
-          <label>
-            Don’t fill this out: <input name="bot-field" onChange={handleChange} />
-          </label>
-        </p>
-        <input placeholder="Name" type="text" name="name" required></input>
-        <input placeholder="Enter email" type="email" name="email" required></input>
-        <textarea placeholder="Your Message" type="text" name="message" required></textarea>
-        <input className="button submit-form is-pulled-right" type="submit" id="submit" value="SUBMIT"></input>
-      </form>
-  )
+    handleChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    render() {
+        const { name, email, message } = this.state;
+        return (
+            <form
+                className="contact-form"
+                name="contact"
+                method="post"
+                action="#"
+                data-netlify="true"
+                data-netlify-honeypot="bot-field"
+                onSubmit={this.handleSubmit}
+            >
+                    {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+                    <input type="hidden" name="form-name" value="contact" />
+  
+                    <input placeholder="Name" type="text" name="name" value={name} onChange={this.handleChange} required></input>
+                    <input placeholder="Enter email" type="email" name="email" value={email} onChange={this.handleChange} required></input>
+                    <textarea placeholder="Your Message" type="text" name="message" value={message} onChange={this.handleChange} required></textarea>
+                    <div id="success">
+                        <div className="success-message">
+                            Your message was sent successfully. Thanks!
+                        </div>
+                    </div>
+                    <input className="button submit-form is-pulled-right" type="submit" id="submit" value="SUBMIT"></input>
+            </form>
+        )
+    }
 }
 
 
